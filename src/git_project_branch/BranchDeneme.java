7bc22a004b92a5a9dad678f3658b8b6ad207018d
bulut67 <mahirbulut67@gmail.com>
2020-08-16 19:29:43 +0300

    branch deneme

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -3,8 +3,11 @@ package git_project_branch;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("saat 19.28 ilk commit branch");
+		
+		
+		
 	}
 
 }
